5bbf8f05349ffc1fa5344e49974a31366f0e99d5
ibroxim2010 <ibrohimazamatov64@gmail.com>
2024-02-21 14:24:09 +0500

    Mening birinchi Javascript loyiham

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = prompt ("Ismingizni kiriting");
+var b = prompt ("Familiyangizni kiriting");
+
+
+
+console.log ( "Sizning ismingizda" , a.length , "ta harf bor" );
+console.log ( "Sizning familiyangizda" , b.length , "ta harf bor" );
+console.log ( "Sizning ismingizda jami bo'lib " , +a.length + +b.length , "ta harf bor " );
+
